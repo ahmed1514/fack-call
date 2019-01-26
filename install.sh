e655947bac38d2db765cc32d5199136141f993be
ahmed1514 <ahmed1514@msn.com>
2019-01-26 22:54:33 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,111 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#Created on 01/1/2019 by Ahmed Al-Qhoom
+#Last update 20/1/2019
+
+setterm -foreground red
+clear
+echo "Installing."
+sleep 1
+clear
+echo "Installing ./ "
+setterm -foreground green
+sleep 1
+clear
+echo "Installing ..\ "
+setterm -foreground red
+sleep 1
+clear
+echo "Installing .../ "
+setterm -foreground green
+sleep 1
+clear
+echo "Installing ....\ "
+setterm -foreground red
+sleep 1
+clear
+echo "Installing ...../ "
+setterm -foreground green
+sleep 1
+clear
+echo "Installing ......\ "
+setterm -foreground red
+sleep 1
+clear
+echo "Installing ......./ "
+setterm -foreground green
+sleep 1
+clear
+echo "Installing ........\ "
+setterm -foreground red
+sleep 1
+clear
+echo "Installing ........./ "
+setterm -foreground green
+sleep 1
+clear
+echo "Installing ..........\ "
+setterm -foreground red
+sleep 1
+dpkg --configure -a &> /dev/null
+
+
+chmod 777 $HOME/fack-call/call.php &> /dev/null
+
+clear
+echo "Ahmed Al-Qhoom is coming Pleas Wait ..."
+apt -y update &> /dev/null
+apt -y upgrade &> /dev/null
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ./ "
+setterm -foreground red
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ..\ "
+setterm -foreground green
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming .../ "
+setterm -foreground red
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ....\ "
+setterm -foreground green
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ...../ "
+setterm -foreground red
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ......\ "
+setterm -foreground green
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ......./ "
+setterm -foreground red
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ........\ "
+setterm -foreground green
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ........./ "
+setterm -foreground red
+sleep 1
+clear
+echo "Ahmed Al-Qhoom is coming ..........\ "
+setterm -foreground green
+echo " "
+sleep 1
+echo " Wait 60(S)"
+apt install -y python2 &> /dev/null
+apt install -y python2-pip &> /dev/null
+apt install -y python3 &> /dev/null
+apt install -y php &> /dev/null
+pip2 install -y requests &> /dev/null
+
+apt install -y figlet &> /dev/null
+
+sleep 1
+clear
+figlet "< DONE >"
